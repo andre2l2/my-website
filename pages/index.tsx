145dbdaddd0bcbb5e72d51eb6ea01f3beb94bcc6
Andre Ferreira <delcferreira@gmail.com>
2022-04-06 22:13:30 -0300

    :fire: testing mata tegs

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,15 +20,20 @@ const IndexPage: React.FC = () => {
 		<Fragment>
 			<HeadOf>
 				<link rel="icon" href="/images/logo.svg" />
-				<link rel="manifest" href="/manifest.json"></link>
+				<link rel="manifest" href="/manifest.json" />
 				<meta property="og:title" content="My-Website" />
-				<meta property="og:description" content="" />
+				<meta property="og:type" content="video.movie" />
+				<meta property="og:description" content="Hello everyone!" />
 				<meta
 					property="og:image"
 					content="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lookandlearn.com%2Fhistory-images%2Fpreview%2FA%2FA007%2FA007160_Gnu.jpg&f=1&nofb=1"
-				></meta>
+				/>
 				<meta
 					property="og:url"
+					content="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lookandlearn.com%2Fhistory-images%2Fpreview%2FA%2FA007%2FA007160_Gnu.jpg&f=1&nofb=1"
+				/>
+				<meta
+					name="twitter:image"
 					content="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lookandlearn.com%2Fhistory-images%2Fpreview%2FA%2FA007%2FA007160_Gnu.jpg&f=1&nofb=1"
 				/>
 			</HeadOf>
