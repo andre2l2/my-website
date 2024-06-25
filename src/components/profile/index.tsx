3a8842e1e450bd0f2a792cb639f26e84d24cbd23
Andre Ferreira <delcferreira@gmail.com>
2024-06-25 13:23:57 -0300

    fix: image bug

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -6,7 +6,7 @@ import { Image, Section } from './style';
 export const Profile: React.FC = () => {
 	return (
 		<Section>
-			<Image src="/images/profile.png" />
+			<Image src="/images/profile.png" width={200} height={200} />
 			<Title type="h1">Olá! É ótimo em ver você aqui</Title>
 		</Section>
 	);
