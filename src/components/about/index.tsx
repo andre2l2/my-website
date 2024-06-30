3c3324dbeae1abafabad7e986d2c7a2b565280a1
Andre Ferreira <delcferreira@gmail.com>
2024-06-30 13:21:00 -0300

    refactor: update texts and packages version
    Was update packages for fixing bugs and update text about me

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { DivSection, H2, P, Section } from './style';
 
-export const About: React.FC = () => {
+type Items = {
+	title: string;
+	details: string;
+};
+
+type AboutPops = {
+	items: Items[];
+};
+
+export const About: React.FC<AboutPops> = ({ items }) => {
 	return (
 		<Section id="about">
-			<DivSection>
-				<H2>Um pouco mais de mim</H2>
-				<P>
-					Me chamo Andre Ferreira, nasci em Alfenas - MG, a terra pão de queijo e do pastel de fubá, mas atualmente,
-					moro no Litoral Norte de São Paulo. Curso Engenharia de Computação e no tempo livre ando de skate, tiro fotos
-					dos lugares onde passo, leio alguns livros, e tenho um bom gosto para música, acredite!
-				</P>
-			</DivSection>
-			<DivSection>
-				<H2>Minha jornada</H2>
-				<P>
-					Meu primeiro trabalho foi como animador de festa infantil e fiz isso durante todo meu ensino médio. E já fui
-					voluntário da Escola da Família (Programa do governo para crianças carentes) aqui da Cidade onde moro. Quando
-					entrei na faculdade, logo fui atrás de estágio na prefeitura aqui da cidade, ajudava no Arquivo tomando conta
-					do arquivamento de processos administrativos. Meu primeiro contato com programação foi com pascal, mas a
-					primeira linguagem que aprendi realmente foi python em 2017, em 2019 descobri o nosso tão amado javascript, e
-					desde então é a linguagem que mais domino, junto de alguns de seus frameworks. No início de 2021 fui recrutado
-					para o time da Olho no Carro e é onde trabalho atualmente. Sou apaixonado por tecnologia, tanto hardware como
-					software, adoro ensinar o que aprendi.
-				</P>
-			</DivSection>
+			{items.map((item) => (
+				<DivSection>
+					<H2>{item.title}</H2>
+					<P>{item.details}</P>
+				</DivSection>
+			))}
 		</Section>
 	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,18 @@ const index: React.FC = () => {
 			<Header />
 			<Main>
 				<Profile />
-				<About />
+				<About
+					items={[
+						{
+							title: 'Um pouco mais de mim',
+							details: `Me chamo Andre Ferreira, nasci em Alfenas - MG, a terra pão de queijo e do pastel de fubá. Atualmente moro no Litoral Norte de São Paulo. Curso Engenharia de Computação e no tempo livre ando de skate, tiro fotos dos lugares onde passo, leio livros, e tenho um otimo gosto musical, acredite!`,
+						},
+						{
+							title: 'Minha jornada',
+							details: `Meu primeiro trabalho foi como animador de festa infantil e fiz isso durante todo meu ensino médio. E já fui voluntário da Escola da Família (Programa do governo para crianças carentes) aqui da Cidade onde moro. Quando entrei na faculdade, logo fui atrás de estágio na prefeitura aqui da cidade, ajudava no Arquivo tomando conta do arquivamento de processos administrativos. Meu primeiro contato com programação foi com pascal, mas a primeira linguagem que aprendi realmente foi python em 2017, em 2019 descobri o nosso tão amado javascript, e desde então é a linguagem que mais domino, junto de alguns de seus frameworks. No início de 2021 fui recrutado para o time da Olho no Carro e é onde trabalho atualmente. Sou apaixonado por tecnologia, tanto hardware como software.`,
+						},
+					]}
+				/>
 				<Card />
 				<Social />
 			</Main>
